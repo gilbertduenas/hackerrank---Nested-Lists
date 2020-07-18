9c3d9dbddabc265ce96ca144c075961bf7c37d82
Gilbert <gilbertduenas@users.noreply.github.com>
2020-07-18 09:03:45 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,5 @@
 # frankencode....
-
+# v2 cleaner with a set
 if __name__ == '__main__':
     roster = {}
     for _ in range(int(input())):
@@ -8,11 +8,8 @@ if __name__ == '__main__':
 
         roster[name] = score
 
-    last = min(roster, key=roster.get)
-    roster.pop(last) 
+    second_last_score = sorted(set(roster.values()))[1]
+    second_last_name = sorted([k for k, v in roster.items() if v == second_last_score])
 
-    second = min(roster.values())
-    second_to_last = sorted([k for k, v in roster.items() if v == second])
-
-    for i in second_to_last:
+    for i in second_last_name:
         print(i)
